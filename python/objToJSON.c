58ee8c9b4f8cab203cb0b8734c639990cbafc8a9
Bulat Gaifullin <gaifullinbf@gmail.com>
2014-03-18 20:18:38 +0300

    fixed indents

--- python/objToJSON.c
+++ python/objToJSON.c
@@ -674,14 +674,14 @@ void Object_beginTypeContext (JSOBJ _obj, JSONTypeContext *tc)
   else
   if (PyIter_Check(obj))
   {
-		PRINTMARK();
-		tc->type = JT_ARRAY;
-		pc->iterBegin = Iter_iterBegin;
-		pc->iterEnd = Iter_iterEnd;
-		pc->iterNext = Iter_iterNext;
-		pc->iterGetValue = Iter_iterGetValue;
-		pc->iterGetName = Iter_iterGetName;
-		return;
+    PRINTMARK();
+    tc->type = JT_ARRAY;
+    pc->iterBegin = Iter_iterBegin;
+    pc->iterEnd = Iter_iterEnd;
+    pc->iterNext = Iter_iterNext;
+    pc->iterGetValue = Iter_iterGetValue;
+    pc->iterGetName = Iter_iterGetName;
+    return;
   }
 
   toDictFunc = PyObject_GetAttrString(obj, "toDict");

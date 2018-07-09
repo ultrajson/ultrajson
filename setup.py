46363a1f5ab78feb578845a8d5a5b41238d23954
lihan <tclh123@gmail.com>
2018-07-09 17:52:41 +0800

    fixup dconv_wrapper linkage
    - remove lib double-conversion building, no need to build it separately.
    otherwise, when there's a system-wide double-conversion lib, it's hard
    to specific this bundled/wrapped double-conversion lib.
    - some format fixup

--- setup.py
+++ setup.py
@@ -1,18 +1,12 @@
 try:
-  from setuptools import setup, Extension
+    from setuptools import setup, Extension
 except ImportError:
-  from distutils.core import setup, Extension
-import distutils.sysconfig
-from distutils.sysconfig import customize_compiler
-from distutils.command.build_clib import build_clib
-from distutils.command.build_ext import build_ext
+    from distutils.core import setup, Extension
 import os.path
 import re
-import sys
 from glob import glob
 
-CLASSIFIERS = filter(None, map(str.strip,
-"""
+CLASSIFIERS = filter(None, map(str.strip, """ \
 Development Status :: 5 - Production/Stable
 Intended Audience :: Developers
 License :: OSI Approved :: BSD License
@@ -24,43 +18,21 @@ Programming Language :: Python :: 3.2
 Programming Language :: Python :: 3.4
 """.splitlines()))
 
-source_files = glob("./deps/double-conversion/double-conversion/*.cc")
-source_files.append("./lib/dconv_wrapper.cc")
-
-libdoubleconversion = (
-    'double-conversion',
-    dict(
-        sources = source_files,
-        include_dirs = ["./deps/double-conversion/double-conversion"],
-        language = "c++"
-    )
-)
-
-
-class build_clib_without_warnings(build_clib):
-    def build_libraries(self, libraries):
-        customize_compiler(self.compiler)
-
-        try:
-            self.compiler.compiler_so.remove("-Wstrict-prototypes")
-        except (AttributeError, ValueError):
-            pass
-
-        build_clib.build_libraries(self, libraries)
-
+dconv_source_files = glob("./deps/double-conversion/double-conversion/*.cc")
+dconv_source_files.append("./lib/dconv_wrapper.cc")
 
 module1 = Extension(
     'ujson',
-     sources = [
-         './python/ujson.c',
-         './python/objToJSON.c',
-         './python/JSONtoObj.c',
-         './lib/ultrajsonenc.c',
-         './lib/ultrajsondec.c'
-     ],
-     include_dirs = ['./python', './lib'],
-     extra_compile_args = ['-D_GNU_SOURCE'],
-     extra_link_args = ['-lstdc++', '-lm']
+    sources=dconv_source_files + [
+        './python/ujson.c',
+        './python/objToJSON.c',
+        './python/JSONtoObj.c',
+        './lib/ultrajsonenc.c',
+        './lib/ultrajsondec.c'
+    ],
+    include_dirs=['./python', './lib', './deps/double-conversion/double-conversion'],
+    extra_compile_args=['-D_GNU_SOURCE'],
+    extra_link_args=['-lstdc++', '-lm']
 )
 
 
@@ -82,22 +54,20 @@ f = open('README.rst')
 try:
     README = f.read()
 finally:
-    f.close()    
-    
+    f.close()
+
 
 setup(
-    name = 'ujson',
-    version = get_version(),
-    description = "Ultra fast JSON encoder and decoder for Python",
-    long_description = README,
-    libraries = [libdoubleconversion],
-    ext_modules = [module1],
+    name='ujson',
+    version=get_version(),
+    description="Ultra fast JSON encoder and decoder for Python",
+    long_description=README,
+    ext_modules=[module1],
     author="Jonas Tarnstrom",
     author_email="jonas.tarnstrom@esn.me",
     download_url="http://github.com/esnme/ultrajson",
     license="BSD License",
     platforms=['any'],
     url="http://www.esn.me",
-    cmdclass = {'build_ext': build_ext, 'build_clib': build_clib_without_warnings},
     classifiers=CLASSIFIERS,
 )

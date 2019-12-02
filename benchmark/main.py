e21a9ac85b732c7f61646434b0f2119a50bf1841
Antonio Cuni <anto.cuni@gmail.com>
2019-12-02 18:46:22 +0100

    tweak the benchmark

--- benchmark/main.py
+++ benchmark/main.py
@@ -1,16 +1,21 @@
+import sys
+CPYTHON = not hasattr(sys, 'pypy_version_info')
 import time
-from tests.support import import_ujson_universal
 import json
 import ujson
-## import ujson_hpy
-## ujson_hpy_universal = import_ujson_universal()
-## ujson_hpy_universal.__name__ = 'ujson_hpy_universal'
+
+from tests.support import import_ujson_universal
+ujson_hpy_universal = import_ujson_universal()
+ujson_hpy_universal.__name__ = 'ujson_hpy_universal'
+
+if CPYTHON:
+    import ujson_hpy
 
 
 def benchmark(mod, fname, N):
     a = time.time()
     for i in range(N):
-        with open(fname) as f:
+        with open(fname, 'rb') as f:
             for line in f:
                 mod.loads(line)
     b = time.time()
@@ -19,12 +24,14 @@ def benchmark(mod, fname, N):
 
 
 def main():
-    N = 10
+    N = 100
     fname = 'benchmark/2015-01-01-15.json'
     benchmark(json, fname, N)
-    benchmark(ujson, fname, N)
-    benchmark(ujson_hpy, fname, N)
     benchmark(ujson_hpy_universal, fname, N)
+    benchmark(ujson, fname, N)
+    if CPYTHON:
+        benchmark(ujson_hpy, fname, N)
+
 
 if __name__ == '__main__':
     main()

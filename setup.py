450b86d8c3bdc1d630850368328bb05459cb9c8a
Hugo <hugovk@users.noreply.github.com>
2020-02-14 21:00:55 +0200

    Add support for Python 3.7 and 3.8

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
 """.splitlines()))
 
 source_files = glob("./deps/double-conversion/double-conversion/*.cc")

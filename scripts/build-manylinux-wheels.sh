ac0a8b4a18243eb1c210fc542a48aaf2687425b9
bwoodsend <bwoodsend@gmail.com>
2020-05-08 17:13:50 +0100

    Remove sudo in comment.

--- scripts/build-manylinux-wheels.sh
+++ scripts/build-manylinux-wheels.sh
@@ -3,11 +3,11 @@ set -e -x
 
 # This is to be run by Docker inside a Docker image.
 # You can test it locally on a Linux machine by installing docker and running from this repo's root:
-# $ sudo docker run  -e PLAT=manylinux1_x86_64  -v `pwd`:/io  quay.io/pypa/manylinux1_x86_64   /io/scripts/build-manylinux-wheels.sh
+# $ docker run  -e PLAT=manylinux1_x86_64  -v `pwd`:/io  quay.io/pypa/manylinux1_x86_64   /io/scripts/build-manylinux-wheels.sh
 
 # The -e just defines an evironment variable PLAT=[docker name] inside the docker - auditwheel can't detect the docker name automatically.
 # The -v gives a directory alias for passing files in and out of the docker. (/io is arbitrary). E.g the setup.py script can be accessed in the docker via /io/setup.py
-# quay.io/pypa/manylinux1_x86_64 is the full docker image name. docker downloads it automatically.
+# quay.io/pypa/manylinux1_x86_64 is the full docker image name. Docker downloads it automatically.
 # The last argument is a shell command that the Docker will execute. Filenames must be from the Docker's perspective.
 
 # Wheels are initially generated as you would usually, but put in a temp directory temp-wheels.

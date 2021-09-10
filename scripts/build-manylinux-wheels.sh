d3fcca81506c2b40fc09f46b8f1326115d476fbf
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-09-10 12:28:22 +0300

    Build slow aarch64 in parallel, build faster x86_64 and i686 in serial

--- scripts/build-manylinux-wheels.sh
+++ scripts/build-manylinux-wheels.sh
@@ -4,7 +4,7 @@ set -e -x
 # This is to be run by Docker inside a Docker image.
 # You can test it locally on a Linux machine by installing Docker and running from this
 # repo's root:
-# $ docker run -v `pwd`:/io quay.io/pypa/manylinux2014_x86_64 /io/scripts/build-manylinux-wheels.sh
+# $ docker run -v `pwd`:/io quay.io/pypa/manylinux2014_x86_64 /io/scripts/build-manylinux-wheels.sh [cp310-cp310]
 
 # The -v gives a directory alias for passing files in and out of the Docker.
 # (/io is arbitrary). E.g the setup.py script can be accessed in the Docker via
@@ -22,13 +22,27 @@ mkdir -p /io/temp-wheels
 # Clean out any old existing wheels.
 find /io/temp-wheels/ -type f -delete
 
+# Log the Python versions in the image.
 ls /opt/python/
-for PYBIN in /opt/python/cp3{6..10}*/bin; do
-    "${PYBIN}/pip" install -q -U setuptools wheel pytest --cache-dir /io/pip-cache
-    (cd /io/ && "${PYBIN}/python" -m pip install .)
-    (cd /io/ && "${PYBIN}/python" -m pytest)
-    (cd /io/ && "${PYBIN}/python" setup.py -q bdist_wheel -d /io/temp-wheels)
-done
+
+build_wheel () {
+  PYBIN=$1
+  "${PYBIN}/pip" install -q -U setuptools wheel pytest --cache-dir /io/pip-cache
+  (cd /io/ && "${PYBIN}/python" -m pip install .)
+  (cd /io/ && "${PYBIN}/python" -m pytest)
+  (cd /io/ && "${PYBIN}/python" setup.py -q bdist_wheel -d /io/temp-wheels)
+}
+
+if [ -n "$1" ]; then
+  # Build a single wheel
+  PYBIN=/opt/python/$1/bin
+  build_wheel ${PYBIN}
+else
+  # Build many wheels
+  for PYBIN in /opt/python/cp3{6..10}*/bin; do
+    build_wheel ${PYBIN}
+  done
+fi
 
 "$PYBIN/pip" install -q auditwheel
 

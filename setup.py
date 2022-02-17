fbae6a31ce55db84eeed2ef8f2c88e62c66ba3fe
Ben Beasley <code@musicinmybrain.net>
2022-02-17 14:38:09 -0500

    Support dynamically linking against system double-conversion library (#508)
    Add env vars to build with system double-conversion. Fixes #376 and is useful to Linux distribution packagers.
    
    New environment variables UJSON_BUILD_DC_INCLUDES and UJSON_BUILD_DC_LIBS allow overriding the include path for double-conversion and adding linker flags for an external double-conversion library. They should generally be used together.

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 import platform
+import shlex
 from glob import glob
-from os import environ
+from os import environ, pathsep
 
 from setuptools import Extension, setup
 
-dconv_source_files = glob("./deps/double-conversion/double-conversion/*.cc")
+dconv_includes = [
+    dir
+    for dir in environ.get(
+        "UJSON_BUILD_DC_INCLUDES",
+        "./deps/double-conversion/double-conversion",
+    ).split(pathsep)
+    if dir
+]
+dconv_libs = shlex.split(environ.get("UJSON_BUILD_DC_LIBS", ""))
+dconv_source_files = []
+if not dconv_libs:
+    dconv_source_files.extend(glob("./deps/double-conversion/double-conversion/*.cc"))
 dconv_source_files.append("./lib/dconv_wrapper.cc")
 
 if platform.system() == "Linux" and environ.get("UJSON_BUILD_NO_STRIP", "0") not in (
@@ -25,9 +37,9 @@ module1 = Extension(
         "./lib/ultrajsonenc.c",
         "./lib/ultrajsondec.c",
     ],
-    include_dirs=["./python", "./lib", "./deps/double-conversion/double-conversion"],
+    include_dirs=["./python", "./lib"] + dconv_includes,
     extra_compile_args=["-D_GNU_SOURCE"],
-    extra_link_args=["-lstdc++", "-lm"] + strip_flags,
+    extra_link_args=["-lstdc++", "-lm"] + dconv_libs + strip_flags,
 )
 
 with open("python/version_template.h") as f:

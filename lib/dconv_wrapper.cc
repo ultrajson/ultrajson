9288c9b571b1416155ba2bb6fe9d23673ac9416f
eritque0arcus <eritque-arcus@ikuyo.dev>
2024-04-29 01:20:27 -0400

    fix: avoid standard version required

--- lib/dconv_wrapper.cc
+++ lib/dconv_wrapper.cc
@@ -28,7 +28,7 @@ namespace double_conversion
   {
     int dconv_d2s(double value, char* buf, int buflen, int* strlength, int allow_nan)
     {
-        static auto d2s = DoubleToStringConverter(DCONV_D2S_EMIT_TRAILING_DECIMAL_POINT | DCONV_D2S_EMIT_TRAILING_ZERO_AFTER_POINT | DCONV_D2S_EMIT_POSITIVE_EXPONENT_SIGN,
+        static DoubleToStringConverter d2s(DCONV_D2S_EMIT_TRAILING_DECIMAL_POINT | DCONV_D2S_EMIT_TRAILING_ZERO_AFTER_POINT | DCONV_D2S_EMIT_POSITIVE_EXPONENT_SIGN,
                  "Infinity", "NaN", 'e', DCONV_DECIMAL_IN_SHORTEST_LOW, DCONV_DECIMAL_IN_SHORTEST_HIGH, 0, 0);
         StringBuilder sb(buf, buflen);
         int success =  static_cast<int>(d2s.ToShortest(value, &sb));
@@ -39,7 +39,7 @@ namespace double_conversion
 
     double dconv_s2d(const char* buffer, int length, int* processed_characters_count)
     {
-        static auto s2d = StringToDoubleConverter(DCONV_S2D_ALLOW_TRAILING_JUNK, 0.0, 0.0, "Infinity", "NaN");
+        static StringToDoubleConverter s2d(DCONV_S2D_ALLOW_TRAILING_JUNK, 0.0, 0.0, "Infinity", "NaN");
         return s2d.StringToDouble(buffer, length, processed_characters_count);
     }
   }
